9dd4d9948e9fd3110477ec2d244e4aadd4c9fe8a
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2021-01-25 14:43:09 +0000

    Content brief section

--- components/content/Brief.vue
+++ components/content/Brief.vue
@@ -40,10 +40,16 @@ export default {
 <style lang="scss" scoped>
 .section {
   @include container;
-  @include spacer-medium-top;
-
+  margin-top: 40px;
+  margin-bottom: 40px;
+  
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+
+  @include breakpoint($small) {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
 }
 
 .h2 { 
@@ -53,17 +59,33 @@ export default {
 }
 
 .content {
-  grid-column: first / end;
+  margin-top: 40px;
 
-  @include breakpoint($small) { grid-column: 3 / end; }
+  grid-column: 1 / 6;
+
+  @include breakpoint($small) { 
+    margin-left: 20px;
+
+    align-self: center;
+    grid-column: 3 / 6; 
+  }
+
   @include breakpoint($medium) { grid-column: 3 / span 2; }
 }
 
 .image {
-  grid-column: first / end;
+  width: 50%;
+  
+  grid-column: 1 / 6;
+  justify-self: center;
 
-  // @include breakpoint($small) { grid-column: first / span 2; }
-  @include breakpoint($medium) { grid-column: 2 / 3; }
+  @include breakpoint($small) { 
+    width: 100%;
+
+    grid-column: 2 / 3;
+
+    transform: translateY(20%);
+  }
 }
 
 .link {
@@ -75,5 +97,6 @@ export default {
   color: $primary;
   font-size: 18px;
   font-weight: bold;
+  margin-bottom: 18px;
 }
 </style>
